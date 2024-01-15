d8d83a47a51683df5d6e4f360db78457c5e8171e
Francis Kornu <fkay0450@gmail.com>
2024-01-15 18:38:22 +0000

    refact: changes to main page

--- src/App.js
+++ src/App.js
@@ -20,14 +20,11 @@ function App() {
       <section className='banner'>
         <section className='grid grid-cols-1 container banner-content-height'>
           <h1 className='white-color banner-text'>
-          Your home away from home.
+            Government Contracting Solutions
           </h1>
-          <p className='white-color banner-p'>
-            Choose one of our locations for your temporary stay.
-          </p>
-          {/* <a target={'blank'} href="https://www.peerspace.com/pages/listings/6359479cab68cd000ef2124c?utm_source=copy_link&utm_campaign=listing_sharing" class="banner-button"><span className='banner-button-text'>Book Now !!</span></a> */}
+          
           <div className='flex flex-col mt-12'>
-            <p className='white-color contact-address contact-phone'>Contact for Booking</p>
+            <p className='white-color contact-address contact-phone'>Contact Us</p>
             <div className='flex flex-row'>
               <img className='w-5 h-6' src={phone} />
               <a href="tel:240-310-9795" className='white-color contact-address under ml-4'>240-310-9795</a>
@@ -37,17 +34,12 @@ function App() {
        
       </section>
       <section className='section-1 container bg-gradient-to-b from-black mb-12'>
-        <h1 className='section-1-text'>Choose one of Holiday homes locations.</h1>
-        <ul class="tile-container">
-          <li class="tile">
-            <img src={img1}/>
-          </li>
-          <li class="tile">
-            <img src={img2}/>
-          </li>
-          
-        </ul>
-       
+        <h1 className='section-1-text underline'>Services.</h1>
+        <div className='flex flex-col'>
+          <h1 className='section-1-text'>* Facility maintenance services</h1>
+          <h1 className='section-1-text'>* Short Term Rentals</h1>
+          <h1 className='section-1-text'>* Government Consulting</h1>
+        </div>
       </section>
       <section className='section-2 container'>
         <div className='grid grid-cols-1 lg:grid-cols-2 lg:p-32 md:p-32'>
@@ -56,37 +48,28 @@ function App() {
           </div>
           <div className='flex flex-col section-2-about-container'>
             <p className='section-2-top'>ABOUT<span className='red-color'>.</span></p>
-            <p className='section-2-heading'>HolydayHomes' Identity</p>
+            <p className='section-2-heading'>About us</p>
             <p className='section-2-p'>
-              HolydayHomes was Founded in 2021,<br></br>
-              We are a business focused on providing travel professionals with short term rental accommodations
+              A trusted provider of government consulting services tailored to meet the diverse needs of both local and federal agencies. At Holyday Homes, we pride ourselves on delivering exceptional solutions that empower government organizations to achieve their objectives efficiently and effectively.
             </p>
             <hr></hr>
-            {/* <div className='flex flex-row mt-8'>
-              <img className='w-12 h-12' src={quoteImg}/>
-              <span className='flex flex-col ml-8'>
-                <p>I learned a long time ago that trying to micromanage the perfect vacation is always a disaster. That leads to terrible times<span className='red-color'>.</span></p>
-                <p className='mt-2'>- Anthony Bourdain</p>
-              </span>
-            </div> */}
           </div>
         </div>
       </section>
       <section className='contact-section bg-gradient-to-t from-black'>
         <div className='grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 bg-white content-center'>
           <div className='p-8'>
-            <p className='contact-heading'>Contacts</p>
-            <p>Use our contact form for all information requests or contact us directly using the contact information below.
-            <br></br><br></br>Feel free to get in touch with us via email or phone</p>
-            <br></br>
-            <div className='flex flex-row'>
+            <p className='contact-heading'>Contacts</p>          
+            <div className='flex flex-row mb-4'>
               <img className='w-8 h-8' src={emailImg} />
               <a href='mailto:Holydayhomes21@gmail.com' className='red-color ml-2 underline cursor-pointer' >Holydayhomes21@gmail.com</a>
-
+            </div>
+            <div className='flex flex-row'>
+              <img className='w-8 h-8' src={phone} />
+              <a href="tel:240-310-9795" className='red-color contact-address under ml-4'>240-310-9795</a>
             </div>
           </div>
           <div className='p-8'>
-            <p className='contact-heading blue-color'>Be the first to know about new spaces.</p>
             <p className='contact-newsletter'>Send us an email</p>
             <img className='email-gif' src={sendEmailImg}/>
           </div>
